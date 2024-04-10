4e17860d349b3343947c4ae4dc5d06b3430de3ab
heitzenrateral320 <153318630+heitzenrateral320@users.noreply.github.com>
2024-04-10 13:52:11 -0400

    made changes

---
+++ javascript_template/main.js
@@ -0,0 +1,7 @@
+const numInches = 72;
+const conversionFactor = 2.54;
+
+const numCentimeters = numInches * conversionFactor;
+
+const myOutput = `${numInches} inches is equivalent to ${numCentimeters} centimeters.`;
+console.log(myOutput)
---
+++ template_strings/main.js
@@ -0,0 +1,7 @@
+const firstName = "Alex";
+const lastName = "Heitzenrater";
+const age = 17;
+const petName = "Zoey";
+const outputString = `My name is ${firstName} ${lastName}, I am ${age} years old, and I have a dog named ${petName}.`;
+
+console.log(outputString)

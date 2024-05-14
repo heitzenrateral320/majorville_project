afe685364eae4d67e8c0779709e3c347075f9697
heitzenrateral320 <153318630+heitzenrateral320@users.noreply.github.com>
2024-05-14 13:44:11 -0400

    completed assignments

---
+++ arrow_functions/main.js
@@ -0,0 +1,9 @@
+// Alex Heitzenrater
+// 5/7/24
+// JS Arrow Functions Practice
+
+const quotient = (num1, num2) => num1 / num2;
+
+const numOne = 100
+const numTwo =5
+console.log(`The quotient of ${numOne} and ${numTwo} is: ${quotient(numOne, numTwo)}`) 
